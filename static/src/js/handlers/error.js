3563b0b7e568e7ce36ec2d46a5b2fd21c8481038
hit9 <hit9@icloud.com>
2015-10-24 22:02:55 +0800

    Add error handler

---
+++ static/src/js/handlers/error.js
@@ -0,0 +1,21 @@
+/**
+ * @overview  Error handler.
+ * @author    Chao Wang (hit9)
+ * @copyright 2015 Eleme, Inc. All rights reserved.
+ */
+
+app.handler('error', function(self, util) {
+  var selector = "#error";
+  var template = $("#template-error").html();
+  /**
+   * Render fatal error
+   * @param {Error} err
+   */
+  self.fatal = function(err) {
+    var html = nunjucks.renderString(template, {
+      type: 'danger',
+      msg: err.toString()
+    });
+    return $(selector).html(html);
+  };
+});
