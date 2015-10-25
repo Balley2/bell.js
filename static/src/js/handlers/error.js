e2528154bcc0b8cc237768f3357953d805f7c865
hit9 <hit9@icloud.com>
2015-10-25 12:32:17 +0800

    Add error,warn,info etc to client-side error handler

--- static/src/js/handlers/error.js
+++ static/src/js/handlers/error.js
@@ -8,16 +8,52 @@ app.handler('error', function(self, util) {
   var selector = "#error";
   var template = $("#template-error").html();
   /**
+   * Render error message.
+   * @param {Error} err
+   * @param {String} className
+   */
+  self.render = function(err, className) {
+    if (!err)
+      return;
+    var html = nunjucks.renderString(template, {
+      type: className,
+      msg: err.toString()
+    });
+    return $(selector).html(html);
+  };
+  /**
    * Render fatal error
    * @param {Error} err
    */
   self.fatal = function(err) {
-    if (!err)
-      return;
-    var html = nunjucks.renderString(template, {
-      type: 'danger',
-      msg: err.toString()
-    });
-    return $(selector).html(html);
+    return self.render(err, 'danger');
+  };
+  /**
+   * Render error
+   * @param {Error} err
+   */
+  self.error = function(err) {
+    return self.render(err, 'danger');
+  };
+  /**
+   * Render warning
+   * @param {Error} err
+   */
+  self.warn = function(err) {
+    return self.render(err, 'warning');
+  };
+  /**
+   * Render info
+   * @param {Error} err
+   */
+  self.info = function(err) {
+    return self.render(err, 'info');
+  };
+  /**
+   * Render success
+   * @param {Error} err
+   */
+  self.ok = function(err) {
+    return self.render(err, 'success');
   };
 });
