ef31b249a94e1ca72c5157e050e1e531c1fcd059
hit9 <hit9@icloud.com>
2015-10-27 03:41:12 +0800

    Incompleted project edit implementation

--- static/src/js/controllers/project.js
+++ static/src/js/controllers/project.js
@@ -8,6 +8,7 @@ app.controller('project', function(self, handlers, util) {
   self.init = function() {
     self.initProjectCreate();
     self.listProject();
+    self.initProjectEdit();
   };
   //-----------------------------------------
   // DOM
@@ -21,6 +22,12 @@ app.controller('project', function(self, handlers, util) {
   dom.project.list = {};
   dom.project.list.tbody = $('.project-list table tbody');
   dom.project.list.template = $('#template-project-node');
+  dom.project.edit = {};
+  dom.project.edit.modal = $('.project-edit .modal');
+  dom.project.edit.error = $('.project-edit .error');
+  dom.project.edit.form = {};
+  dom.project.edit.form.projectName = $('.project-edit form.project-name');
+  dom.project.edit.form.projectDelete = $('.project-edit button.project-delete');
   //-----------------------------------------
   // Project Create
   //-----------------------------------------
@@ -35,8 +42,8 @@ app.controller('project', function(self, handlers, util) {
           return;
         }
         dom.project.create.modal.modal('hide');
-        handlers.error.ok('Project created');
         self.appendProject(project);
+        handlers.error.ok('Project created');
       });
     });
     // modal on hide
@@ -53,7 +60,12 @@ app.controller('project', function(self, handlers, util) {
       project: project,
       url: util.url,
     });
-    return dom.project.list.tbody.append(node);
+    dom.project.list.tbody.append(node);
+    var selector = util.format(".project-list table a[data-name*='%s']", project.name);
+    $(selector).click(function(e) {
+      e.preventDefault();
+      dom.project.edit.modal.modal('show', $(this));
+    });
   };
   self.listProject = function() {
     handlers.project.getAll(function(err, projects) {
@@ -62,6 +74,29 @@ app.controller('project', function(self, handlers, util) {
         return;
       }
       projects.forEach(self.appendProject);
+    });
+  };
+  //-----------------------------------------
+  // Project edit
+  //-----------------------------------------
+  self.initProjectEdit = function() {
+    self.initProjectEditModal();
+  };
+  self.initProjectEditModal = function() {
+    dom.project.edit.modal.on('show.bs.modal', function(e) {
+      var el = $(e.relatedTarget);
+      var name = el.data('name');
+      self.fillProjectEditModal(name);
+    });
+  };
+  self.fillProjectEditModal = function(name) {
+    handlers.project.get(name, function(err, project) {
+      if (err) {
+        handlers.error.error(err, dom.project.edit.error);
+        return;
+      }
+      util.fillForm(dom.project.edit.form.projectName, project);
+      // FIXME (list rules and receivers)
     });
   };
 });
