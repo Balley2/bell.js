cfcbde7fc2a3a4acc0df0a89a0fb46a72e1dbf1f
hit9 <hit9@icloud.com>
2015-10-28 17:27:41 +0800

    Simlify project view handler etc

--- static/src/js/controllers/project.js
+++ static/src/js/controllers/project.js
@@ -5,111 +5,63 @@
  */
 
 app.controller('project', function(self, handlers, util) {
-  self.init = function() {
-    self.initProjectCreate();
-    self.listProject();
-    self.initProjectEdit();
-  };
-  //-----------------------------------------
-  // DOM
-  //-----------------------------------------
+  //------------------------------------------------------
+  // HTML DOM Selectors
+  //------------------------------------------------------
   var dom = {};
-  dom.project = {};
-  dom.project.create = {};
-  dom.project.create.modal = $('.project-create .modal');
-  dom.project.create.form = $('.project-create form');
-  dom.project.create.error = $('.project-create .error');
-  dom.project.list = {};
-  dom.project.list.tbody = $('.project-list table tbody');
-  dom.project.list.template = $('#template-project-node');
-  dom.project.edit = {};
-  dom.project.edit.modal = $('.project-edit .modal');
-  dom.project.edit.error = $('.project-edit .error');
-  dom.project.edit.form = {};
-  dom.project.edit.form.projectName = $('.project-edit form.project-name');
-  dom.project.edit.form.projectDelete = $('.project-edit button.project-delete');
-  //-----------------------------------------
-  // Project Create
-  //-----------------------------------------
-  self.initProjectCreate = function() {
-    // form on submit
-    dom.project.create.form.submit(function(e) {
+  dom = {};
+  dom.create = {};
+  dom.create.form = $('.project-create form');
+  dom.list = {};
+  dom.list.table = $('.project-list tbody');
+  dom.list.template = $('.project-list #template-project-node');
+  //------------------------------------------------------
+  // Events Handlers
+  //------------------------------------------------------
+  /**
+   * Entry.
+   */
+  self.init = function() {
+    self.list();
+    self.initCreate();
+  };
+  /**
+   * Append project to list.
+   */
+  self.append = function(project) {
+    var template = dom.list.template.html();
+    var node = nunjucks.renderString(template, {
+      project: project,
+      url: util.url
+    });
+    dom.list.table.append(node);
+  };
+  /**
+   * List projects
+   */
+  self.list = function(project) {
+    handlers.project.getAll(function(err, projects) {
+      if (err) {
+        handlers.error.error(err);
+        return;
+      }
+      projects.forEach(self.append);
+    });
+  };
+  /**
+   * Init create.
+   */
+  self.initCreate = function() {
+    dom.create.form.submit(function(e) {
       e.preventDefault();
       var data = util.collectForm(this);
       handlers.project.create(data.name, function(err, project) {
         if (err) {
-          handlers.error.error(err, dom.project.create.error);
+          handlers.error.error(err);
           return;
         }
-        dom.project.create.modal.modal('hide');
-        self.appendProject(project);
-        handlers.error.ok('Project created');
-      });
-    });
-    // modal on hide
-    dom.project.create.modal.on('hidden.bs.modal', function(e) {
-      dom.project.create.error.html('');
-    });
-  };
-  //-----------------------------------------
-  // Project List
-  //-----------------------------------------
-  self.appendProject = function(project) {
-    var template = dom.project.list.template.html();
-    var node = nunjucks.renderString(template, {
-      project: project,
-      url: util.url,
-    });
-    dom.project.list.tbody.append(node);
-    var selector = util.format(".project-list table a[data-id*='%d']", project.id);
-    $(selector).click(function(e) {
-      e.preventDefault();
-      dom.project.edit.modal.modal('show', $(this));
-    });
-  };
-  self.listProject = function() {
-    handlers.project.getAll(function(err, projects) {
-      if (err)  {
-        handlers.error.error(err);
-        return;
-      }
-      projects.forEach(self.appendProject);
-    });
-  };
-  //-----------------------------------------
-  // Project edit
-  //-----------------------------------------
-  self.initProjectEdit = function() {
-    self.initProjectEditModal();
-  };
-  self.initProjectEditModal = function() {
-    dom.project.edit.modal.on('show.bs.modal', function(e) {
-      var el = $(e.relatedTarget);
-      var id = el.data('id');
-      self.fillProjectEditModal(id);
-      self.initProjectEditName(id);
-    });
-  };
-  self.fillProjectEditModal = function(id) {
-    handlers.project.get(id, function(err, project) {
-      if (err) {
-        handlers.error.error(err, dom.project.edit.error);
-        return;
-      }
-      util.fillForm(dom.project.edit.form.projectName, project);
-      // FIXME (list rules and receivers)
-    });
-  };
-  self.initProjectEditName = function(id) {
-    dom.project.edit.form.projectName.submit(function(e) {
-      e.preventDefault();
-      var data = util.collectForm(this);
-      handlers.project.patch(id, data.name, function(err, data) {
-        if (err) {
-          handlers.error.error(err, dom.project.edit.error);
-          return;
-        }
-        handlers.error.ok("project name updated", dom.project.edit.error);
+        self.append(project);
+        handlers.error.ok("Project created");
       });
     });
   };
