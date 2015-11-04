02d67ec7bd7f7ebc732cd278dc6d74700d52d250
hit9 <hit9@icloud.com>
2015-11-04 16:34:37 +0800

    Add param `project` to sendEmail and sendSms

--- exampleSender.js
+++ exampleSender.js
@@ -4,13 +4,20 @@
 
 const util = require('util');
 
-exports.sendEmail = function(email, data, log) {
+/**
+ * @param {String} email
+ * @param {Object} project
+ * @param {Object} data
+ * @param {Logger} log
+ */
+exports.sendEmail = function(email, project, data, log) {
+  var projectName = project.name;
   var date = new Date(data.stamp * 1000).toTimeString().slice(0, 8);
   var value = data.value.toFixed(2);
   var trend = data.trend.toFixed(2);
   // format message and send email
 };
 
-exports.sendSms = function(phone, data, log) {
+exports.sendSms = function(phone, project, data, log) {
   // similar with `sendEmail`
 };
